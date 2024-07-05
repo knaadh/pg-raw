584d6536313a6a1b0dabeb723ab152aa3cf14c4c
Mithlesh Jasrotia <mithleshjs@gmail.com>
2024-07-06 00:22:32 +0530

    fix: missing update fn

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export * from "./lib/delete";
 export * from "./lib/find";
 export * from "./lib/insert";
 export * from "./lib/types";
+export * from "./lib/update";
 export { pgFn } from "./lib/util";
